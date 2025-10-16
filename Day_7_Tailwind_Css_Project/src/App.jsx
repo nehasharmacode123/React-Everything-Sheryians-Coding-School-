54bd89abe5d08955005766bf03726ef3b5789549
nehasharmacode123 <nehasharma962595@gmail.com>
2025-10-16 23:52:22 +0530

    day_9

--- Day_7_Tailwind_Css_Project/src/App.jsx
+++ Day_7_Tailwind_Css_Project/src/App.jsx
@@ -1,6 +1,8 @@
 const App = () => {
   return (
-    <div className="h-screen w-full bg-gray-900">App</div>
+    <div className="h-screen w-full bg-gray-900">
+      <div className="dog">Hello Neha</div>
+    </div>
   )
 }
 
